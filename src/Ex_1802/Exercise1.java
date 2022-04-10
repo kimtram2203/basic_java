9da31442f561379fba33c503adf1896c8f0532b5
kimtram2203 <19t1021276@husc.edu.vn>
2022-04-10 20:48:14 +0700

    18-02 Exercise1 y = ax + b

---
+++ src/Ex_1802/Exercise1.java
@@ -0,0 +1,22 @@
+package Ex_1802;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		var input = new Scanner(System.in); 
+		System.out.println("y = ax + b");
+		
+		System.out.print("input a: ");
+		a = input.nextInt();
+		
+		System.out.print("input b: ");
+		b = input.nextInt();
+		
+		System.out.print(a == 0 ? "Phuong trinh co vo so nghiem" : ("Phuong trinh co nghiem" + (-b/a)));
+	}
+
+}
