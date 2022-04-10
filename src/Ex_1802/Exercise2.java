8ec68c79b0200d7551550fbf48039451c97f0587
kimtram2203 <19t1021276@husc.edu.vn>
2022-04-10 21:01:42 +0700

    no message

---
+++ src/Ex_1802/Exercise2.java
@@ -0,0 +1,18 @@
+package ex_1802;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r;
+		var input = new Scanner(System.in);
+		
+		System.out.print("input r: ");
+		r = input.nextInt();
+		
+		System.out.print("Chu vi duong tron la: " + 2 * 3.14 * r);
+	}
+
+}
